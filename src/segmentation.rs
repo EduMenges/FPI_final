d40db29eee6ed0592c0d152ce78421aab432eb95
EduMenges <eduardo.mattje@gmail.com>
2023-02-14 10:23:51 -0300

    Side scanning is done

--- src/segmentation.rs
+++ src/segmentation.rs
@@ -12,7 +12,7 @@ pub type Coordinates = (u16, u16);
 pub fn segment_img(img: &GrayAlphaImage) -> ImageSegments {
     let mut segments = ImageSegments::new();
 
-    let mut visited = vec![false; (img.width() * img.height()) as usize];
+    let mut visited = VisitedPixels::new((img.width() as u16, img.height() as u16));
 
     let mut pixel_stack: Vec<Coordinates> = Vec::new();
 
@@ -20,10 +20,39 @@ pub fn segment_img(img: &GrayAlphaImage) -> ImageSegments {
 
     while !pixel_stack.is_empty() {
         let coords = pixel_stack.pop().unwrap();
-        let tone = img.get_pixel(coords.0 as u32, coords.1 as u32).0;
     }
 
     segments
+}
+
+fn tone_to_range(img: &GrayAlphaImage, coords: Coordinates) -> Range<u16> {
+    let mut lower = side_scan(img, coords, -1);
+    let mut upper = side_scan(img, coords, 1);
+
+    lower..=upper
+}
+
+fn side_scan(img: &GrayAlphaImage, coords: Coordinates, increment: i8) -> u16 {
+    let mut res = coords.0;
+    let tone = img.get_pixel(coords.0 as u32, coords.1 as u32).0;
+
+    if tone[1] > 0 {
+        loop {
+            let left_neighbour =
+                img.get_pixel_checked(coords.0 + increment as u32, coords.1 as u32);
+
+            match left_neighbour {
+                Some(pixel) => {
+                    if pixel.0 == tone && pixel.1 > 0 {
+                        res -= 1;
+                    }
+                }
+                None => break,
+            }
+        }
+    }
+
+    res
 }
 
 struct VisitedPixels {
@@ -33,7 +62,10 @@ struct VisitedPixels {
 
 impl VisitedPixels {
     pub fn new(dimensions: Coordinates) -> Self {
-        Self { visited: Vec::with_capacity(dimensions.0 as usize * dimensions.1 as usize), dimensions }
+        Self {
+            visited: Vec::with_capacity(dimensions.0 as usize * dimensions.1 as usize),
+            dimensions,
+        }
     }
 
     pub fn visit_tone(&mut self, coords: Coordinates) {
