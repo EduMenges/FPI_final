a67eff538dec611995630e7a3abd3b4bfd8eddf5
Eduardo Mattje <eduardomattje@gmail.com>
2023-02-14 21:21:04 -0300

    Segmentation should be done

--- src/segmentation.rs
+++ src/segmentation.rs
@@ -1,58 +1,114 @@
 use std::{
     collections::{HashMap, LinkedList, VecDeque},
-    ops::Range,
+    ops::RangeInclusive,
 };
 
-use image::GrayAlphaImage;
+use image::{GenericImageView, GrayAlphaImage};
 
-pub type ImageSegment = HashMap<u16, Vec<Range<u16>>>;
+pub type ImageSegment = HashMap<u16, Vec<RangeInclusive<u16>>>;
 pub type ImageSegments = LinkedList<ImageSegment>;
 pub type Coordinates = (u16, u16);
 
-pub fn segment_img(img: &GrayAlphaImage) -> ImageSegments {
-    let mut segments = ImageSegments::new();
+enum Direction {
+    Left,
+    Right,
+}
 
-    let mut visited = VisitedPixels::new((img.width() as u16, img.height() as u16));
+struct ImgSegmentation<'a> {
+    pub visited: VisitedPixels,
+    pub segments: ImageSegments,
+    pub pixel_stack: Vec<Coordinates>,
+    img: &'a GrayAlphaImage,
+}
 
-    let mut pixel_stack: Vec<Coordinates> = Vec::new();
+impl<'a> ImgSegmentation<'a> {
+    pub fn segment_img(img: &'a GrayAlphaImage) -> ImageSegments {
+        let segments = ImageSegments::new();
+        let visited = VisitedPixels::new((img.width() as u16, img.height() as u16));
+        let pixel_stack: Vec<Coordinates> = Vec::new();
+        let mut this = Self {
+            visited,
+            segments,
+            pixel_stack,
+            img,
+        };
 
-    pixel_stack.push((0, 0));
+        this.pixel_stack.push((0, 0));
 
-    while !pixel_stack.is_empty() {
-        let coords = pixel_stack.pop().unwrap();
+        while !this.pixel_stack.is_empty() {
+            let coords = this.pixel_stack.pop().unwrap();
+
+            if !this.visited.is_visited(coords) {
+                let mut new_segment = ImageSegment::new();
+                this.mount_segment(&mut new_segment, coords);
+                this.segments.push_front(new_segment);
+            }
+        }
+
+        this.segments
     }
 
-    segments
-}
+    fn mount_segment(&mut self, new_segment: &mut ImageSegment, coords: Coordinates) {
+        let tone_range = self.tone_to_range(coords);
+        let tone = self.img.get_pixel(coords.0 as u32, coords.1 as u32).0[0];
 
-fn tone_to_range(img: &GrayAlphaImage, coords: Coordinates) -> Range<u16> {
-    let mut lower = side_scan(img, coords, -1);
-    let mut upper = side_scan(img, coords, 1);
+        new_segment
+            .entry(coords.1)
+            .and_modify(|v| v.push(tone_range.clone()))
+            .or_insert_with(|| vec![tone_range.clone()]);
 
-    lower..=upper
-}
-
-fn side_scan(img: &GrayAlphaImage, coords: Coordinates, increment: i8) -> u16 {
-    let mut res = coords.0;
-    let tone = img.get_pixel(coords.0 as u32, coords.1 as u32).0;
-
-    if tone[1] > 0 {
-        loop {
-            let left_neighbour =
-                img.get_pixel_checked(coords.0 + increment as u32, coords.1 as u32);
-
-            match left_neighbour {
-                Some(pixel) => {
-                    if pixel.0 == tone && pixel.1 > 0 {
-                        res -= 1;
-                    }
-                }
-                None => break,
+        if coords.1 != 0 && coords.1 as u32 != self.img.height() {
+            for x in tone_range {
+                self.mount_next_line((x, coords.1 - 1), tone, new_segment);
+                self.mount_next_line((x, coords.1 + 1), tone, new_segment);
             }
         }
     }
 
-    res
+    fn mount_next_line(&mut self, coords: (u16, u16), tone: u8, new_segment: &mut ImageSegment) {
+        if !self.visited.is_visited(coords)
+            && self.img.get_pixel(coords.0 as u32, coords.1 as u32).0[0] == tone
+        {
+            self.mount_segment(new_segment, coords);
+        }
+    }
+
+    fn tone_to_range(&mut self, coords: Coordinates) -> RangeInclusive<u16> {
+        let lower = self.side_scan(coords, Direction::Left);
+        let upper = self.side_scan(coords, Direction::Right);
+
+        lower..=upper
+    }
+
+    fn side_scan(&mut self, coords: Coordinates, direction: Direction) -> u16 {
+        let mut res = coords.0;
+        let tone = self.img.get_pixel(coords.0 as u32, coords.1 as u32).0;
+        let walk = match direction {
+            Direction::Left => |coord| coord - 1,
+            Direction::Right => |coord| coord + 1,
+        };
+
+        if tone[1] > 0 {
+            loop {
+                if res != 0 && res as u32 != self.img.width() {
+                    res = walk(res);
+
+                    let neighbour = self.img.get_pixel(res as u32, coords.1 as u32);
+
+                    if neighbour.0[0] == tone[0] && neighbour.0[1] > 0 {
+                        self.visited.visit_tone((res, coords.1));
+                        res -= 1;
+                    } else {
+                        self.pixel_stack.push(coords);
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        res
+    }
 }
 
 struct VisitedPixels {
@@ -70,5 +126,9 @@ impl VisitedPixels {
 
     pub fn visit_tone(&mut self, coords: Coordinates) {
         self.visited[coords.0 as usize + (coords.1 * self.dimensions.0) as usize] = true;
+    }
+
+    pub fn is_visited(&self, coords: Coordinates) -> bool {
+        self.visited[coords.0 as usize + (coords.1 * self.dimensions.0) as usize]
     }
 }
